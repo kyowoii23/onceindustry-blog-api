ca97880017fd11377079192e9b4215cc4e1676d6
kyowoii23 <kyowoii23@gmail.com>
2022-08-01 17:21:59 +0900

    Message API 작업

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,7 @@ import express, {Request, Response} from 'express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import {initializeApp} from 'firebase-admin/app';
-import { firestore } from 'firebase-admin';
-
+import {MessageRouter} from './router/MessageRouter';
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,27 +12,12 @@ app.use(morgan('combined'));
 initializeApp();
 
 app.get('/', (req: Request, res: Response) => {
-  const name = process.env.NAME || 'World';
-  res.send(`Hello My ${name}!`);
+  res.send('Hello World!');
 });
-
-app.get('/message', async (req: Request, res: Response) => {
-  const messageRef = firestore().collection('message');
-  const snapshot = await messageRef.get();
-  const dataset = snapshot.docs.map(doc => {
-    return doc.data();
-  })
-  res.status(200).send(dataset);
-})
-
-app.post('/message', async (req: Request, res: Response) => {
-  const messageRef = firestore().collection('message');
-  messageRef.add({message: 'hello world'});
-  res.status(200).send('message added');
-})
-
+app.use(MessageRouter);
 
 const port = parseInt(`${process.env.PORT}`) || 8080;
+
 app.listen(port, () => {
   console.log(`helloworld: listening on port ${port}`);
 });
